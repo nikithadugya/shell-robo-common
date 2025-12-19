6f9ba8bab82ba507f67b2559fedb31208a9946e7
nikithadugya <nikithadugya@gmail.com>
2025-12-19 17:22:36 +0530

    ssssssssssss

---
+++ catalogue.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=catalogue
+
+app_setup ### here app_setup doesnot require any dependency on nodejs_setup so first
+nodejs_setup
+
+systemd_setup
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copy mongo repo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Install MONGODB Client"
+
+#Checking whether the schema catalogue already present in the database mongo to avoid duplicate
+INDEX=$(mongosh mongodb.dawsnikitha.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalog')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load $app_name produvts i.e, Loading Schemas from backend to database"
+else
+    echo -e "$app_name products already loaded... $Y SKIPPING $N"
+fi
+
+systemctl restart $app_name
+
+print_total_time
--- common.sh
+++ common.sh
@@ -12,6 +12,8 @@ LOGS_FOLDER="/var/log/shell-robo"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 ) # $0 --> Current file name  --> 14-logs
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 START_TIME=$(date +%s)
+MONGODB_HOST="mongodb.dawsnikitha.fun"
+
 
 mkdir -p $LOGS_FOLDER  # -p means if directory is not there it creates if directory is there is keeps quiet
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE  # tee command appends the output printing of this command on mobexterm same in LOg file as well.
@@ -33,6 +35,42 @@ else
     echo -e "Installation $2 ... $G success $N"
 fi
 }
+
+
+### NODEJS ###
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Disabling NodeJS"
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "Enabling NodeJS"
+    dnf install nodejs -y
+    VALIDATE $? "Installing NodeJS"
+}
+
+
+##app-setup
+app_setup(){
+    mkdir -p /app  # If already directory is present then -p mentioning ignores and next if not there it creates
+    VALIDATE $? "Creating App Directory"
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "DOwnloading $app_name application"
+    cd /app 
+    VALIDATE $? "Changing to App Directory" # Here we got error because if files already present in app folder then it throws error so before only we are deleting
+    rm -rf /app/*
+    unzip /tmp/$app_name.zip &>>$LOG_FILE
+    VALIDATE $? "Unzip $app_name"
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+    VALIDATE $? "Copy systemctl services"
+    systemctl daemon-reload
+    VALIDATE $? "demon reload"
+    systemctl enable catalogue &>>$LOG_FILE
+    VALIDATE $? "enabling catalogue"
+}
+
+#Printing time
 print_total_time(){
 
 END_TIME=$(date +%s)
